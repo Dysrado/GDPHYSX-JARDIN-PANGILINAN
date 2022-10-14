f0283acf78f7e777e50c134d4afd3af814012482
Dysrado <63017345+Dysrado@users.noreply.github.com>
2022-10-14 21:43:22 +0800

    Bug Fixes in Model3d Class
    Edited equations to make it more efficient in the particle class

--- GL/GL/Model3D.cpp
+++ GL/GL/Model3D.cpp
@@ -7,16 +7,28 @@ void Model3D::initVariables(glm::vec3 pos, glm::vec3 size, glm::vec3 rot, int ty
 	// Sets the Model3D's location, scale and rotation to the parameters location, scale and rotation
 	switch (type) {
 		case 0:
-			this->acceleration = inverseMass * glm::vec3(0, 150.0f, 3000.0f);
+			this->velocity = glm::vec3(0, 0, 35.0f);
+			this->inverseMass = 50.0f;
+			this->acceleration = glm::vec3(0,-1.0f, 0);
+			this->damping = 0.99f;
 			break;
 		case 1:
-			this->acceleration = inverseMass * glm::vec3(0, 3000.0f, 3000.0f);
+			this->velocity = glm::vec3(0,30.0f, 40.0f);
+			this->inverseMass = 1.0f;
+			this->acceleration = glm::vec3(0, -20.0f, 0);
+			this->damping = 0.99f;
 			break;
 		case 2:
-			this->acceleration = inverseMass * glm::vec3(0, 1.0f, 1.0f);
+			this->velocity = glm::vec3(0, 0, 10.0f);
+			this->inverseMass = 5.0f;
+			this->acceleration = glm::vec3(0, 0.6f, 0);
+			this->damping = 0.9f;
 			break;
 		case 3:
-			this->acceleration = inverseMass * glm::vec3(0, 0, 300.0f);
+			this->velocity = glm::vec3(0, 0, 100.0f);
+			this->inverseMass = 100.0f;
+			this->acceleration = glm::vec3(0, -1.0f, 0);
+			this->damping = 0.99f;
 			break;
 
 	}
@@ -72,66 +84,10 @@ void Model3D::integrate(float duration, int type)
 {
 
 	if (duration > 0.0) {
-		//std::cout << "Duration:" << duration << std::endl;
-		
-		//print();
+		positionUpdate(duration);
+		velocity += acceleration * duration;
+		velocity *= pow(damping, duration);
 
-		switch (type) {
-			case 0: //Pistol Bullet
-				positionUpdate(duration); //Updates the position of the projectile based on the time and velocity.
-				acceleration.y -= gravity;
-				velocity += acceleration * duration;
-				velocity.z *= pow(damping, duration);
-				
-				 //* glm::vec3(0, force, force); 
-				//acceleration.y *= -gravity;
-				//velocity = acceleration * duration * duration * 0.5f;
-				//acceleration.y *= -gravity * duration;
-
-				//velocity.y = -gravity * duration;
-				
-				/*std::cout << "Velocity.x = " << velocity.x;
-				std::cout << " Velocity.y = " << velocity.y;
-				std::cout << " Velocity.z = " << velocity.z << std::endl;*/
-				
-
-				break;
-			case 1: //Artillery Bullet
-				//positionUpdate(duration); //Updates the position of the projectile based on the time and velocity.
-				//acceleration = inverseMass * glm::vec3(0, force + -gravity, force);
-				////acceleration.y *= inverseMass * -gravity.y;
-				//velocity = acceleration * duration * duration * 0.5f;
-				//velocity.z *= damping;
-				positionUpdate(duration); //Updates the position of the projectile based on the time and velocity.
-				acceleration.y -= gravity;
-				velocity += acceleration * duration;
-				velocity.z *= pow(damping, duration);
-				break;
-			case 2: //Fireball
-				positionUpdate(duration); //Updates the position of the projectile based on the time and velocity.
-				velocity += acceleration * duration;
-				velocity.z *= pow(damping, duration);
-				break;
-			case 3: //Laser
-				positionUpdate(duration); //Updates the position of the projectile based on the time and velocity.
-				velocity += acceleration * duration;
-				break;
-			
-
-		}
-
-		
-		//velocity = acceleration * duration * duration * 0.5f;
-		/*acceleration.z = inverseMass * force;
-		
-		acceleration = inverseMass * glm::vec3(force/2,0,force);
-		acceleration.y = inverseMass * -gravity.y;*/
-		
-
-
-		//velocity.x *= damping;
-		//velocity.y *= damping;
-		//velocity.z *= damping;
 		
 		transform = glm::translate(glm::mat4(1.0f), position);
 	}
@@ -140,7 +96,6 @@ void Model3D::integrate(float duration, int type)
 
 void Model3D::positionUpdate(float duration)
 {
-	//this->position += velocity * duration + acceleration * duration * duration * 0.5f;
 	position += velocity * duration;
 
 
@@ -169,10 +124,5 @@ void Model3D::deleteVertex() {
 	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
 }
-void Model3D::print()
-{
-	std::cout << "Position X: " << this->position.x << std::endl
-		<< "Position Y: " << this->position.y << std::endl
-		<< "Position Z: " << this->position.z << std::endl;
-}
+
 ;

b2b8cab83c5577f072a540b7bc833fd42eaf6e3c
Wayne Michael Pangilinan <113033667+Makiiidon@users.noreply.github.com>
2022-10-28 21:03:41 +0800

    Added ParticleForceRegistry Skeleton
    Base format for the ParticleForceRegistry

---
+++ GL/GL/ParticleForceRegistry.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Particle.h"
+#include "ParticleForceGenerator.h"
+#include <vector>
+
+class ParticleForceRegistry
+{
+protected:
+	struct ParticleForceRegistration {
+		Particle* particle;
+		ParticleForceGenerator* fg;
+	};
+
+	typedef std::vector<ParticleForceRegistration> Registry;
+	Registry registrations;
+
+public:
+	void add(Particle *particle, ParticleForceGenerator *fg);
+	void remove(Particle *particle, ParticleForceGenerator *fg);
+
+	void clear();
+
+	void updateForces(float duration);
+};
+
